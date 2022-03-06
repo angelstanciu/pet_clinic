b9e4e25a40dafc967f723cddd4d331c3ee47f9cf
AdrianDobos <adriano_dobosh@yahoo.com>
2022-03-06 14:55:00 +0200

    4 - adaugare interfata BaseRepository

---
+++ src/main/java/BaseRepository.java
@@ -0,0 +1,12 @@
+import java.util.Optional;
+
+public interface BaseRepository<T, ID> {
+        Optional<T> findById(ID id);
+
+        void save(T entity);
+
+        void update(T entity);
+        
+        void delete(T entity);
+    }
+
