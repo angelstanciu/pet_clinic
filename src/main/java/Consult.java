5c18f86a68914a235bdf8a412c823781941402c6
LuizaDobos <badica_luiza@yahoo.com>
2022-03-06 14:19:07 +0200

    am adaugat consult

--- src/main/java/Consult.java
+++ src/main/java/Consult.java
@@ -1,7 +1,31 @@
 public class Consult {
-    veterinarian Veterinarian = new Veterinarian;
+    private Veterinarian veterinarian;
+    private Pet pet;
     private String date;
-    private String descripion;
+    private String description;
+
+    public Consult(Veterinarian veterinarian, Pet pet, String date, String descripion) {
+        this.veterinarian = veterinarian;
+        this.pet = pet;
+        this.date = date;
+        this.description = descripion;
+    }
+
+    public Veterinarian getVeterinarian() {
+        return veterinarian;
+    }
+
+    public void setVeterinarian(Veterinarian veterinarian) {
+        this.veterinarian = veterinarian;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
 
     public String getDate() {
         return date;
@@ -11,19 +35,21 @@ public class Consult {
         this.date = date;
     }
 
-    public String getDescripion() {
-        return descripion;
+    public String getDescription() {
+        return description;
     }
 
-    public void setDescripion(String descripion) {
-        this.descripion = descripion;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
     public String toString() {
         return "Consult{" +
-                "date='" + date + '\'' +
-                ", descripion='" + descripion + '\'' +
+                "veterinarian=" + veterinarian +
+                ", pet=" + pet +
+                ", date='" + date + '\'' +
+                ", description='" + description + '\'' +
                 '}';
     }
 }
