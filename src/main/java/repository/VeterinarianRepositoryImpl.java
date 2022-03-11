e06654a1509df8ae484d2e7a0a93989e061e1d94
L.Adina <gheorghelarisa16@yahoo.com>
2022-03-11 19:26:48 +0200

    am adaugat Veterinarian repo & impl

---
+++ src/main/java/repository/VeterinarianRepositoryImpl.java
@@ -0,0 +1,9 @@
+package repository;
+
+import models.Veterinarian;
+
+public class VeterinarianRepositoryImpl extends BaseRepositoryImpl<Veterinarian,Integer> implements VeterinarianRepository{
+    public VeterinarianRepositoryImpl() {
+        super(Veterinarian.class);
+    }
+}
